cbcce22e259eb4318525bfafc241dbf896f0badb
Amodmathapati <103567778+Amodmathapati@users.noreply.github.com>
2022-05-04 22:05:57 +0530

    Add files via upload

---
+++ amod.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main(){
+int arr[10]={4,5,77,88,99,55,44,20,32,69};
+int key;
+int a;
+
+cout<<"enter the key"<<endl;
+cin>>key;
+for (int i = 0; i < 10; i++)
+{
+    if (arr[i]==key){
+    
+      a=5;     }
+}
+
+if (a==5)
+{
+    cout<<"yes"<<endl;
+}
+else
+cout<<"No"<<endl;
+
+
+return 0;
+}
+
